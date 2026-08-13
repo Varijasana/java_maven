a4882728a95538bcfea8da21caf5608ef0fadea8
Varijasana <varijasana9ascs609078@gmail.com>
2026-08-13 07:48:38 +0530

    Complete executable Maven project

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testAnotherAdd() {
+        assertEquals(10, calculator.add(5, 5));
+    }
+}
